c81cbc59b6dcc4085bec5e69798d5bdb8297e8e9
unknown <yulkuksgauz@mail.ru>
2026-09-21 15:45:56 +0300

    before deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,31 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api/hn': {
-        target: 'https://hacker-news.firebaseio.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/hn/, '/v0'),
-      },
-    },
-  },
   resolve: {
     alias: {
-      '@': path.resolve(import.meta.dirname, './src'),
-      '@components': path.resolve(import.meta.dirname, './src/components'),
-      '@styles': path.resolve(import.meta.dirname, './src/styles'),
-      '@typesal': path.resolve(import.meta.dirname, './src/types'),
-      '@pages': path.resolve(import.meta.dirname, './src/pages'),
-      '@utils': path.resolve(import.meta.dirname, './src/utils'),
-      '@store': path.resolve(import.meta.dirname, './src/store'),
-      '@hooks': path.resolve(import.meta.dirname, './src/hooks'),
-      '@constants': path.resolve(import.meta.dirname, './src/constants'),
-      '@server': path.resolve(import.meta.dirname, './src/server'),
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@typesal': path.resolve(__dirname, './src/types'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@server': path.resolve(__dirname, './src/server'),
     },
   },
 });
